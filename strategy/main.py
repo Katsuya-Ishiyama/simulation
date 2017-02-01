0bb8b7bbdc787f4658a6dc4d61e96c1f17b5e824
katsuya-ishiyama <ishiyama.katsuya@gmail.com>
2017-02-01 09:50:05 +0900

    add logging

--- strategy/main.py
+++ strategy/main.py
@@ -3,22 +3,30 @@
 
 
 from strategy import Strategy
+from utils import logger
+
 
 def simulate_stick_to_one_strategy(sim, n):
 
+    log = logger.SimulationLogger()
+
     for sim_num in range(1, sim+1):
         strategy = Strategy(n)
+        current_strategy = strategy.choose_strategy()
 
-        strategy.choose_strategy()
+        log.set_common_parameters(
+            strategy=current_strategy,
+            sim_num=sim_num
+        )
 
         for trial_num in range(1, n+1):
             is_success = strategy.try_strategy()
 
-            trial_log = strategy.get_trial_log()
-            trial_log['simulation_num'] = sim_num
-            trial_log['trial_num'] = trial_num
-
-            # TODO: 結果ログを書き込む処理を作る
+            log.set_parameters(
+                trial_num=trial_num,
+                result=is_success
+            )
+            log.logging()
 
             if is_success:
                 break
